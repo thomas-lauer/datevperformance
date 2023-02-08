7fc71cbd1d53e5a51d50c7690f69c5b7cd56ffd4
Thomas Lauer <lauer@glsh.net>
2023-02-08 23:50:39 +0100

    Update getcounters.ps1

--- getcounters.ps1
+++ getcounters.ps1
@@ -1,12 +1,24 @@
-$counterlist = @("\Prozessor(_Total)\Prozessorzeit (%)", "\Hyper-V Hypervisor: virtueller Prozessor(_Total)\% Gesamtlaufzeit")
+# Counters für HyperV
+
+$counterlist = @("\Prozessor(_Total)\Prozessorzeit (%)",
+                 "\Arbeitsspeicher\Verfügbare MB",
+                 "\Netzwerkadapter(*)\Gesamtanzahl Bytes/s",
+                 "\Hyper-V-Ausgleichsmodul für dynamischen Arbeitsspeicher(*)\Verfügbarer Arbeitsspeicher",
+                 "\Prozessor(_Total)\% C2-Zeit",
+                 "\Logischer Datenträger(*)\Mittlere Sek./Lesevorgänge",
+                 "\Logischer Datenträger(*)\Mittlere Sek./Schreibvorgänge"
+                 )
+
+#"\Logischer Datenträger(*)\Mittlere Sek./Lesevorgänge"
+#"\Logischer Datenträger(*)\Mittlere Sek./Schreibvorgänge"
+# Wenn genug IOPS vorhanden das bewegen sich diese Werte zwischen 1ms und 15ms 
+# ständig bis 25ms ungünstiger EInfluss auf Applicationen.
+# > 26ms problematisch
+
+
+                 
 
 # $counterlist | Get-Counter -MaxSamples 120 | Export-Counter -Path .\counter.csv -FileFormat csv
 # Gibt man den Parameter FileFormat nicht an, dann verwendet Export-Counter standardmäßig ein binäres Format.
 
-$counterlist | Get-Counter -MaxSamples 120 | Export-Counter -Path .\counter.blg
-$counterlist = @("\Prozessor(_Total)\Prozessorzeit (%)", "\Hyper-V Hypervisor: virtueller Prozessor(_Total)\% Gesamtlaufzeit")
-
-# $counterlist | Get-Counter -MaxSamples 120 | Export-Counter -Path .\counter.csv -FileFormat csv
-# Gibt man den Parameter FileFormat nicht an, dann verwendet Export-Counter standardmäßig ein binäres Format.
-
-$counterlist | Get-Counter -MaxSamples 120 | Export-Counter -Path .\counter.blg
+Get-Counter -Counter $counterlist -MaxSamples 120 | Export-Counter -Path .\counter.blg
