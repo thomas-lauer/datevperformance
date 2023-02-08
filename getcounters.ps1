2d375daddb0335ace1e1473b47939fd164e485c3
Thomas Lauer <lauer@glsh.net>
2023-02-08 23:21:26 +0100

    Update getcounters.ps1

--- getcounters.ps1
+++ getcounters.ps1
@@ -1,5 +1,12 @@
+$counterlist = @("\Prozessor(_Total)\Prozessorzeit (%)", "\Hyper-V Hypervisor: virtueller Prozessor(_Total)\% Gesamtlaufzeit")
 
-
-$c | Get-Counter -MaxSamples 120 | Export-Counter -Path .\counter.csv -FileFormat csv
-
+# $counterlist | Get-Counter -MaxSamples 120 | Export-Counter -Path .\counter.csv -FileFormat csv
 # Gibt man den Parameter FileFormat nicht an, dann verwendet Export-Counter standardmäßig ein binäres Format.
+
+$counterlist | Get-Counter -MaxSamples 120 | Export-Counter -Path .\counter.blg
+$counterlist = @("\Prozessor(_Total)\Prozessorzeit (%)", "\Hyper-V Hypervisor: virtueller Prozessor(_Total)\% Gesamtlaufzeit")
+
+# $counterlist | Get-Counter -MaxSamples 120 | Export-Counter -Path .\counter.csv -FileFormat csv
+# Gibt man den Parameter FileFormat nicht an, dann verwendet Export-Counter standardmäßig ein binäres Format.
+
+$counterlist | Get-Counter -MaxSamples 120 | Export-Counter -Path .\counter.blg
