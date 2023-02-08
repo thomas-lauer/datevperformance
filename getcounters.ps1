57a8c56227812e70deaf85692fe1cb2a0bd35398
Thomas Lauer <lauer@glsh.net>
2023-02-09 00:18:09 +0100

    Update getcounters.ps1

--- getcounters.ps1
+++ getcounters.ps1
@@ -6,7 +6,10 @@ $counterlist = @("\Prozessor(_Total)\Prozessorzeit (%)",
                  "\Hyper-V-Ausgleichsmodul für dynamischen Arbeitsspeicher(*)\Verfügbarer Arbeitsspeicher",
                  "\Prozessor(_Total)\% C2-Zeit",
                  "\Logischer Datenträger(*)\Mittlere Sek./Lesevorgänge",
-                 "\Logischer Datenträger(*)\Mittlere Sek./Schreibvorgänge"
+                 "\Logischer Datenträger(*)\Mittlere Sek./Schreibvorgänge",
+                 "\Hyper-V-Hypervisor – logischer Prozessor(_Total)\% Gesamtausführungszeit",
+                 "\Hyper-V-Hypervisor – Stamm des virtuellen Prozessors(_Total)\% Gesamtausführungszeit",
+                 "\Hyper-V-Hypervisor – virtueller Prozessor(*)\% Gesamtausführungszeit"
                  )
 
 #"\Logischer Datenträger(*)\Mittlere Sek./Lesevorgänge"
@@ -15,10 +18,17 @@ $counterlist = @("\Prozessor(_Total)\Prozessorzeit (%)",
 # ständig bis 25ms ungünstiger EInfluss auf Applicationen.
 # > 26ms problematisch
 
+# Wenn der \Hyper-V-Hypervisor – logischer Prozessor(_Total)\% Gesamtausführungszeit > 90% ist der Host überlastet
 
-                 
+# Wenn der "\Hyper-V-Hypervisor – virtueller Prozessor(_Total)\% Gesamtausführungszeit" > 90% dann folgendes prüfen
+# Prüfen, ob der Host überlastet ist
+# Finden Sie heraus, ob die Arbeitslast mehr virtuelle Prozessoren nutzen kann
+# Weisen Sie der virtuellen Maschine mehr virtuelle Prozessoren zu
+   
+# Wenn \Hyper-V-Ausgleichsmodul für dynamischen Arbeitsspeicher(*)\Verfügbarer Arbeitsspeicher
+# und \Arbeitsspeicher\Verfügbare MB der VM mehr speicher geben
 
-# $counterlist | Get-Counter -MaxSamples 120 | Export-Counter -Path .\counter.csv -FileFormat csv
+# $counterlist | Get-Counter -MaxSamples 600 | Export-Counter -Path .\counter.csv -FileFormat csv
 # Gibt man den Parameter FileFormat nicht an, dann verwendet Export-Counter standardmäßig ein binäres Format.
 
 Get-Counter -Counter $counterlist -MaxSamples 120 | Export-Counter -Path .\counter.blg
